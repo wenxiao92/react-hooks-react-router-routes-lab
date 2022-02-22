12ee09837bfba560440e291c3d23730078e16dc8
Wen Xiao <wennstyle@gmail.com>
2022-02-22 12:43:24 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  //console.log(directors)
+  const displayDirectors = directors.map((dir) => {
+    return(
+      <div key={dir.name}>
+      <h1>Name: {dir.name}</h1>
+      <div> Movies:
+      <ul>
+        <div>
+          {dir.movies.map((movie) => {
+            return(
+              <li key={movie}>{movie}</li>
+            )
+          })}
+        </div>
+      </ul>
+      </div>
+      </div>
+    )
+  })
+
+  return <div>{displayDirectors}</div>;
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return( <div className="navbar">
+
+    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink to="/movies">Movies</NavLink></li>
+    <li><NavLink to="/directors">Directors</NavLink></li>
+    <li><NavLink to="/actors">Actors</NavLink></li>
+
+  </div>);
 }
 
 export default NavBar;
